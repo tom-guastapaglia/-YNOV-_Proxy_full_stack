567b55092d7b5253a461ef122e720d614058fa60
FERRALI-Mario <you@example.com>
2023-01-05 16:14:51 +0100

    :building_construction: roots

--- proxy/src/app.ts
+++ proxy/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import Request from 'express';
 import axios from "axios";
 import bodyParser from "body-parser";
 
@@ -19,6 +20,26 @@ app.get("/api/.user", (_, res) => {
   axios.get("http://nginx/api/hello").then((onfulfilled) => {
     res.send(onfulfilled.data);
   });
+});
+
+app.get('/api/hello', (_, res) => {
+  res.send('Hello World');
+});
+
+app.post('/api/.user/login', (_, res) => {
+  res.send('Login');
+});
+
+app.get('/api/.user/user', (_, res) => {
+  res.send('User');
+});
+
+app.get('/api/.user/admin', (_, res) => {
+  res.send('Admin');
+});
+
+app.get('/api/.user/checkRole', (_, res) => {
+  res.send('Role');
 });
 
 app.listen(port, () => {
